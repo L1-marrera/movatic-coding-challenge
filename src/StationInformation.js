5b0e6d6b251d64158b5a97a974ce17a325ca3a33
Luis De La Rosa <luismarrera10@gmail.com>
2023-09-07 15:04:38 -0400

    Added Icon in 2nd page to return to 1st page Changed Station from table to list

--- src/StationInformation.js
+++ src/StationInformation.js
@@ -1,8 +1,10 @@
 import { useParams, useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
+import { AiOutlineLeft } from 'react-icons/ai'
 import './StationInformation.css';
 
 function StationInformation({ selectedRow }) {
+    const history = useNavigate();
     const { id } = useParams();
     const [station_information, setStation_information] = useState('');
     const [error, setError] = useState('');
@@ -17,7 +19,7 @@ function StationInformation({ selectedRow }) {
                 return response.json();
             })
             .then(data => {
-                console.log(data[0])
+                // console.log(data[0])
                 setStation_information(data)
             })
             .catch(error => {
@@ -26,6 +28,10 @@ function StationInformation({ selectedRow }) {
             });
     }, [id]);
 
+    const previousPage = () => {
+        history('/');
+    };
+
     // station_information && console.log(station_information[0]);
 
     return (
@@ -33,6 +39,7 @@ function StationInformation({ selectedRow }) {
             <h2 className="StationInformation-header">Coding Challenge</h2>
             {error && <p>Error: {error}</p>}
             <div className="StationInformation-menu">
+                <button onClick={previousPage}> <AiOutlineLeft /> Back</button>
                 <h3 className="StationInformation-header--id">Station ID: {id}</h3>
             </div>
             <div className="StationInformation-bodyContainer">
@@ -41,31 +48,31 @@ function StationInformation({ selectedRow }) {
                     <p>{station_information[0].name}</p>
                 )}
             </div>
-            <div  className="StationInformation-bodyContainer">
+            <div className="StationInformation-bodyContainer">
                 <p>Address:</p>
                 {station_information && (
                     <p>{station_information[0].address}</p>
                 )}
             </div>
-            <div  className="StationInformation-bodyContainer">
+            <div className="StationInformation-bodyContainer">
                 <p>Region:</p>
                 {station_information && (
                     <p>{station_information[0].region_id}</p>
                 )}
             </div>
-            <div  className="StationInformation-bodyContainer">
+            <div className="StationInformation-bodyContainer">
                 <p>Station Type:</p>
                 {station_information && (
                     <p>{station_information[0]._bcycle_station_type}</p>
                 )}
             </div>
-            <div  className="StationInformation-bodyContainer">
+            <div className="StationInformation-bodyContainer">
                 <p>Longitude:</p>
                 {station_information && (
                     <p>{station_information[0].lon}</p>
                 )}
             </div>
-            <div  className="StationInformation-bodyContainer">
+            <div className="StationInformation-bodyContainer">
                 <p>Latitude:</p>
                 {station_information && (
                     <p>{station_information[0].lat}</p>
--- src/StationTable.js
+++ src/StationTable.js
@@ -6,22 +6,29 @@ function StationTable({ stationJson }) {
     // console.log(tableData);
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>URL</th>
-                </tr>
-            </thead>
-            <tbody>
-                {tableData.map((item) => (
-                    <tr key={item.name}>
-                        <td>{item.name}</td>
-                        <td>{item.url}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        // <table>
+        //     <thead>
+        //         <tr>
+        //             <th>Name</th>
+        //             <th>URL</th>
+        //         </tr>
+        //     </thead>
+        //     <tbody>
+        //         {tableData.map((item) => (
+        //             <tr key={item.name}>
+        //                 <td>{item.name}</td>
+        //                 <td>{item.url}</td>
+        //             </tr>
+        //         ))}
+        //     </tbody>
+        // </table>
+        <ul>
+            {tableData.map((item) => (
+                <li key={item.name}>
+                    <strong> {item.name} </strong>: {item.url}
+                </li>
+            ))}
+        </ul>
     );
 }
 
